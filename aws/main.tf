45e6df84e7dd28562404a6acaf4bb2e7c75b2271
HyejiPyo <gpwl1228@naver.com>
2025-10-10 15:59:08 +0900

    update

--- aws/main.tf
+++ aws/main.tf
@@ -2,21 +2,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "jenkins_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.aws_key_name
-  subnet_id     = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.default.id]
-
-  tags = {
-    Name = "jenkins-server"
-  }
-}
-
 resource "aws_security_group" "default" {
-  name        = "default-sg"
-  description = "기본 보안그룹"
+  name        = "jenkind-cd-sg"
+  description = "jenkins-sg"
   vpc_id      = var.vpc_id
 
   ingress {
@@ -33,6 +21,20 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -40,3 +42,74 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# Jenkins Server(CD)
+resource "aws_instance" "jenkins_server" {
+  ami           = data.aws_ssm_parameter.amazon_linux_2.value
+  instance_type = var.instance_type
+  key_name      = var.aws_key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y openjdk-11-jdk
+    wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+    sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+    sudo apt-get update
+    sudo apt-get install -y jenkins
+    sudo systemctl enable jenkins
+    sudo systemctl start jenkins
+  EOF
+
+  tags = {
+    Name = "jenkins-server"
+  }
+}
+
+# Jenkins Agent
+resource "aws_instance" "jenkins_agent" {
+  ami           = data.aws_ssm_parameter.amazon_linux_2.value
+  instance_type = var.instance_type
+  key_name      = var.aws_key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin/
+  EOF
+
+  tags = {
+    Name = "jenkins-agent"
+  }
+}
+
+# Prometheus Server
+resource "aws_instance" "prometheus" {
+  ami           = data.aws_ssm_parameter.amazon_linux_2.value
+  instance_type = var.instance_type
+  key_name      = var.aws_key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y prometheus
+    sudo systemctl enable prometheus
+    sudo systemctl start prometheus
+  EOF
+
+  tags = {
+    Name = "prometheus-server"
+  }
+}
+
--- gcp/main.tf
+++ gcp/main.tf
@@ -3,8 +3,9 @@ provider "google" {
   region  = var.gcp_region
 }
 
-resource "google_compute_instance" "jenkins_ci" {
-  name         = "jenkins-ci"
+# Jenkins Agent Server(Junit 테스트 필요)
+resource "google_compute_instance" "jenkins_agent" {
+  name         = "jenkins-agent"
   machine_type = var.gcp_machine_type
   zone         = var.gcp_zone
 
@@ -15,7 +16,17 @@ resource "google_compute_instance" "jenkins_ci" {
   }
 
   metadata = {
-    ssh-keys = "${var.gcp_ssh_user}:${var.gcp_ssh_pubkey} ${var.gcp_ssh_user}"
+    ssh-keys = var.gcp_metadata_ssh_keys
+    user-data = <<-EOF
+      #!/bin/bash
+      sudo apt-get update
+      sudo apt-get install -y docker.io
+      sudo systemctl enable docker
+      sudo systemctl start docker
+      curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+      chmod +x kubectl
+      sudo mv kubectl /usr/local/bin/
+    EOF
   }
 
   network_interface {
@@ -23,7 +34,51 @@ resource "google_compute_instance" "jenkins_ci" {
     subnetwork = var.gcp_subnet_self_link
     access_config {}
   }
+
+  tags = ["jenkins-agent"]
 }
+
+# Jenkins Server(CI)
+resource "google_compute_instance" "jenkins_server" {
+  name         = "jenkins-server"
+  machine_type = var.gcp_machine_type
+  zone         = var.gcp_zone
+
+  boot_disk {
+    initialize_params {
+      image = var.gcp_image
+    }
+  }
+
+  metadata = {
+    ssh-keys = var.gcp_metadata_ssh_keys
+    user-data = <<-EOF
+      #!/bin/bash
+      sudo apt-get update
+      sudo apt-get install -y openjdk-11-jdk
+      wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+      sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+      sudo apt-get update
+      sudo apt-get install -y jenkins
+      sudo systemctl enable jenkins
+      sudo systemctl start jenkins
+    EOF
+  }
+
+  network_interface {
+    network    = var.gcp_network_self_link
+    subnetwork = var.gcp_subnet_self_link
+    access_config {}
+  }
+
+  tags = ["jenkins-server"]
+}
+
+  network_interface {
+    network    = var.gcp_network_self_link
+    subnetwork = var.gcp_subnet_self_link
+    access_config {}
+  }
 
 resource "google_compute_firewall" "default" {
   name    = "default-firewall"
@@ -31,8 +86,9 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "80"]
+    ports    = ["22", "80", "8080", "50000"]
   }
 
   source_ranges = ["0.0.0.0/0"]
 }
+
